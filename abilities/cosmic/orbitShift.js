a629248beca8118a8aa063ca0cb75744ab774ebd
novise17 <koffin@nycstudents.net>
2026-05-20 11:48:35 -0400

    Update orbitShift.js

--- abilities/cosmic/orbitShift.js
+++ abilities/cosmic/orbitShift.js
@@ -1,1 +1,11 @@
+import { Ability } from "../Ability.js";
 
+export class OrbitShift extends Ability {
+    constructor() {
+        super({ name: "Orbit Shift", cooldown: 2500, energyCost: 22 });
+    }
+
+    activate(fighter) {
+        fighter.x += fighter.facing * 100; // teleport dash
+    }
+}
