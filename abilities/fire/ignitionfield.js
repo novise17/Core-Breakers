6c185236c37d304af34445f975d3ed70603a10c0
novise17 <koffin@nycstudents.net>
2026-05-20 11:34:04 -0400

    Update ignitionfield.js

--- abilities/fire/ignitionfield.js
+++ abilities/fire/ignitionfield.js
@@ -1,1 +1,23 @@
+import { Ability } from "../Ability.js";
 
+export class IgnitionField extends Ability {
+    constructor() {
+        super({
+            name: "Ignition Field",
+            cooldown: 2000,
+            energyCost: 25
+        });
+    }
+
+    activate(fighter, enemy) {
+        enemy.health -= 5;
+
+        const burn = setInterval(() => {
+            enemy.health -= 2;
+        }, 500);
+
+        setTimeout(() => {
+            clearInterval(burn);
+        }, 3000);
+    }
+}
