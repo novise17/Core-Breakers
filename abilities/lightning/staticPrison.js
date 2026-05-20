3f2a9ebb082d525a99008c372bc328133ae83dc6
novise17 <koffin@nycstudents.net>
2026-05-20 11:43:05 -0400

    Update staticPrison.js

--- abilities/lightning/staticPrison.js
+++ abilities/lightning/staticPrison.js
@@ -1,1 +1,15 @@
+import { Ability } from "../Ability.js";
 
+export class StaticPrison extends Ability {
+    constructor() {
+        super({ name: "Static Prison", cooldown: 2500, energyCost: 25 });
+    }
+
+    activate(fighter, enemy) {
+        enemy.speed = 1;
+
+        setTimeout(() => {
+            enemy.speed = 6;
+        }, 2500);
+    }
+}
