5e2e55dda9d9caadc0fe9a0c22fb96b664f26346
novise17 <koffin@nycstudents.net>
2026-05-20 11:39:00 -0400

    Update Titano.js

--- fighters/Titano.js
+++ fighters/Titano.js
@@ -1,1 +1,23 @@
+import { Fighter } from "./Fighter.js";
 
+import { SeismicCrash } from "../abilities/earth/seismicCrash.js";
+import { GravitonPull } from "../abilities/earth/gravitonPull.js";
+import { StoneMantle } from "../abilities/earth/stoneMantle.js";
+import { FaultLine } from "../abilities/earth/faultLine.js";
+import { Worldbreaker } from "../abilities/earth/worldbreaker.js";
+
+export class Titano extends Fighter {
+    constructor(x, color, controls) {
+        super("Titano", x, color, controls);
+
+        this.abilities = {
+            q: new SeismicCrash(),
+            w: new GravitonPull(),
+            e: new StoneMantle(),
+            r: new FaultLine(),
+            f: new Worldbreaker()
+        };
+
+        this.armor = 0;
+    }
+}
