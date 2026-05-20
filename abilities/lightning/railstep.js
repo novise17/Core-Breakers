9d00e5910961f985d7a78dce7da9db6fd2f4f991
novise17 <koffin@nycstudents.net>
2026-05-20 11:42:02 -0400

    Update railstep.js

--- abilities/lightning/railstep.js
+++ abilities/lightning/railstep.js
@@ -1,1 +1,11 @@
+import { Ability } from "../Ability.js";
 
+export class Railstep extends Ability {
+    constructor() {
+        super({ name: "Railstep", cooldown: 600, energyCost: 10 });
+    }
+
+    activate(fighter) {
+        fighter.x += fighter.facing * 90;
+    }
+}
