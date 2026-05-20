f55452da2fbbbb770215cb2ea9dfb7d297defbed
novise17 <koffin@nycstudents.net>
2026-05-20 11:41:31 -0400

    Update Volt.js

--- fighters/Volt.js
+++ fighters/Volt.js
@@ -1,1 +1,23 @@
+import { Fighter } from "./Fighter.js";
 
+import { Railstep } from "../abilities/lightning/railstep.js";
+import { ArcKnives } from "../abilities/lightning/arcKnives.js";
+import { EMPPulse } from "../abilities/lightning/empPulse.js";
+import { StaticPrison } from "../abilities/lightning/staticPrison.js";
+import { ThunderGodProtocol } from "../abilities/lightning/thunderGodProtocol.js";
+
+export class Volt extends Fighter {
+    constructor(x, color, controls) {
+        super("Volt", x, color, controls);
+
+        this.abilities = {
+            q: new Railstep(),
+            w: new ArcKnives(),
+            e: new EMPPulse(),
+            r: new StaticPrison(),
+            f: new ThunderGodProtocol()
+        };
+
+        this.overcharged = false;
+    }
+}
