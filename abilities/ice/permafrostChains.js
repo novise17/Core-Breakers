ef95fd250c97b1c237fbce87a8d13fdf62fba700
novise17 <koffin@nycstudents.net>
2026-05-20 11:45:26 -0400

    Update permafrostChains.js

--- abilities/ice/permafrostChains.js
+++ abilities/ice/permafrostChains.js
@@ -1,1 +1,12 @@
+import { Ability } from "../Ability.js";
 
+export class PermafrostChains extends Ability {
+    constructor() {
+        super({ name: "Permafrost Chains", cooldown: 2200, energyCost: 22 });
+    }
+
+    activate(fighter, enemy) {
+        enemy.speed = 1;
+        enemy.health -= 8;
+    }
+}
