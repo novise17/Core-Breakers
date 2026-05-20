75714b95d4d0dcedeb696c349d740d75c1ba9cbb
novise17 <koffin@nycstudents.net>
2026-05-20 11:47:39 -0400

    Update pulsarBeam.js

--- abilities/cosmic/pulsarBeam.js
+++ abilities/cosmic/pulsarBeam.js
@@ -1,1 +1,11 @@
+import { Ability } from "../Ability.js";
 
+export class PulsarBeam extends Ability {
+    constructor() {
+        super({ name: "Pulsar Beam", cooldown: 900, energyCost: 12 });
+    }
+
+    activate(fighter, enemy) {
+        enemy.health -= 14;
+    }
+}
