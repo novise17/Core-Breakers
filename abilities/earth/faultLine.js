5e1e8682e4bbf12ffd651a1c7394f097dcfecd52
novise17 <koffin@nycstudents.net>
2026-05-20 11:40:38 -0400

    Update faultLine.js

--- abilities/earth/faultLine.js
+++ abilities/earth/faultLine.js
@@ -1,1 +1,12 @@
+import { Ability } from "../Ability.js";
 
+export class FaultLine extends Ability {
+    constructor() {
+        super({ name: "Fault Line", cooldown: 2200, energyCost: 22 });
+    }
+
+    activate(fighter, enemy) {
+        enemy.health -= 10;
+        enemy.x += fighter.facing * 80;
+    }
+}
