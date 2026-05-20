c1a3999be02085a914f8d44bbc0be121991f11f5
novise17 <koffin@nycstudents.net>
2026-05-20 11:42:19 -0400

    Update arcKnives.js

--- abilities/lightning/arcKnives.js
+++ abilities/lightning/arcKnives.js
@@ -1,1 +1,12 @@
+import { Ability } from "../Ability.js";
 
+export class ArcKnives extends Ability {
+    constructor() {
+        super({ name: "Arc Knives", cooldown: 1000, energyCost: 12 });
+    }
+
+    activate(fighter, enemy) {
+        enemy.health -= 12;
+        enemy.x += fighter.facing * 20;
+    }
+}
