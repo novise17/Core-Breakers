52e5d55e07492bdaa6641542c9796d2746cd8eeb
novise17 <koffin@nycstudents.net>
2026-05-21 10:04:49 -0400

    Update nightfallSlash.js

--- abilities/shadow/nightfallSlash.js
+++ abilities/shadow/nightfallSlash.js
@@ -1,1 +1,12 @@
+import { Ability } from "../Ability.js";
 
+export class NightfallSlash extends Ability {
+    constructor() {
+        super({ name: "Nightfall Slash", cooldown: 1800, energyCost: 20 });
+    }
+
+    activate(fighter, enemy) {
+        enemy.health -= 16;
+        enemy.x += fighter.facing * 50;
+    }
+}
