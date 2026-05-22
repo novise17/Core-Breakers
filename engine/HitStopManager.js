7da86ef8038e46e7cf8e3abf26c964d5cd3e9806
novise17 <koffin@nycstudents.net>
2026-05-22 09:31:31 -0400

    Update HitStopManager.js

--- engine/HitStopManager.js
+++ engine/HitStopManager.js
@@ -1,1 +1,25 @@
+// engine/hitStopManager.js
 
+export class HitStopManager {
+  constructor() {
+    this.timer = 0;
+    this.globalSlow = 1;
+  }
+
+  trigger(frames = 6) {
+    this.timer = Math.max(this.timer, frames);
+  }
+
+  update() {
+    if (this.timer > 0) {
+      this.timer--;
+      this.globalSlow = 0; // freeze simulation
+    } else {
+      this.globalSlow = 1;
+    }
+  }
+
+  isFrozen() {
+    return this.timer > 0;
+  }
+}
